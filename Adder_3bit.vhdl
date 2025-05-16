d9a5e030e3928daf36193edd899e5586d8d22fa4
Dineth Sankalpa <dinethsankalpaofficial@gmail.com>
2025-05-16 23:48:27 +0530

    Update Adder_3bit.vhdl

--- Adder_3bit.vhdl
+++ Adder_3bit.vhdl
@@ -32,34 +32,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Adder_3_Bit is
-    port (
-        A       : in  std_logic_vector(2 downto 0);
-        B       : in  std_logic_vector(2 downto 0);
-        C_in     : in  std_logic;
-        S    : out std_logic_vector(2 downto 0);
-        C_out    : out std_logic
+    Port (
+        A : in  std_logic_vector(2 downto 0);
+        S : out std_logic_vector(2 downto 0)
     );
 end Adder_3_Bit;
 
 architecture Behavioral of Adder_3_Bit is
-    signal G, P  : std_logic_vector(2 downto 0); 
-    signal C_outI : std_logic_vector(1 downto 0);  
-
+    signal C0, C1 : std_logic;
 begin
-    -- Carry Bit Generator and Propergator
-    G <= A and B;
-    P <= A xor B;
-    
-    -- intermediate Carry Outs
-    C_outI(0) <= G(0) or (P(0) and C_in);
-    C_outI(1) <= G(1) or (P(1) and G(0)) or (P(1) and P(0) and C_in);
-    
-    -- intermediate sums
-    S(0) <= P(0) xor C_in;
-    S(1) <= P(1) xor C_outI(0);
-    S(2) <= P(2) xor C_outI(1);
-    
-    -- Carry Out
-    C_out <= G(2) or (P(2) and G(1)) or (P(2) and P(1) and P(0) and C_in);
+    -- LSB
+    S(0) <= not A(0);
+    C0 <= A(0);
+
+    -- Middle bit
+    S(1) <= A(1) xor C0;
+    C1 <= A(1) and C0;
+
+    -- MSB
+    S(2) <= A(2) xor C1;
 
 end Behavioral;
