5b3d377df2c4495f0bdc803b99409a9c0fc1c2a6
Dineth Sankalpa <dinethsankalpaofficial@gmail.com>
2025-05-16 23:47:43 +0530

    Update AddSub_Unit.vhdl

--- AddSub_Unit.vhdl
+++ AddSub_Unit.vhdl
@@ -18,24 +18,14 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Add_Sub_4bit is
     Port ( 
         A_AS : in STD_LOGIC_VECTOR (3 downto 0);
         B_AS : in STD_LOGIC_VECTOR (3 downto 0);
-        Ctrl : in STD_LOGIC; -- to activate Substraction if Ctrl = '1'
+        Ctrl : in STD_LOGIC; -- to activate Subtraction if Ctrl = '1'
         S_AS : out STD_LOGIC_VECTOR (3 downto 0);
         Zero : out STD_LOGIC;
         Overflow : out STD_LOGIC
@@ -45,40 +35,42 @@ end Add_Sub_4bit;
 architecture Behavioral of Add_Sub_4bit is
     signal B_inter, S_inter: STD_LOGIC_VECTOR(3 DOWNTO 0);
     
-    -- RCA
+    -- RCA component declaration with updated port names
     component RCA_4
         port (
             A      : in  STD_LOGIC_VECTOR(3 downto 0);
             B      : in  STD_LOGIC_VECTOR(3 downto 0);
-            C_in   : in  STD_LOGIC;
-            S      : out STD_LOGIC_VECTOR(3 downto 0);
-            C_out  : out STD_LOGIC
+            Cin    : in  STD_LOGIC;
+            SUM    : out STD_LOGIC_VECTOR(3 downto 0);
+            Cout   : out STD_LOGIC
         );
     end component;
 
 begin    
+    -- XOR gates for 2's complement subtraction (when Ctrl = '1')
     B_inter(0) <= B_AS(0) xor Ctrl;
     B_inter(1) <= B_AS(1) xor Ctrl;
     B_inter(2) <= B_AS(2) xor Ctrl;
     B_inter(3) <= B_AS(3) xor Ctrl;
     
-    -- Ripple Carry Adder (4 Bit)
+    -- Ripple Carry Adder (4 Bit) instantiation with updated port mapping
     RCA_4_0 : RCA_4
         port map (
-            A => A_AS,
-            B => B_inter,
-            C_in => Ctrl,
-            S => S_inter,
-            C_out => Overflow
+            A   => A_AS,
+            B   => B_inter,
+            Cin => Ctrl,      
+            SUM => S_inter,  
+            Cout => Overflow  
         );
 
+    -- Zero flag detection
     process (S_inter)
-        begin
-            if (S_inter = "0000") then
-                Zero <= '1';
-            else
-                Zero <= '0';
-            end if;
+    begin
+        if (S_inter = "0000") then
+            Zero <= '1';
+        else
+            Zero <= '0';
+        end if;
     end process;
     
     S_AS <= S_inter;
